2e2e40eb1e71ca95299af8304fb9ab6d1a988ab3
SabrinaCat <Sabrina.glow22@gmail.com>
2020-07-17 21:16:42 -0500

    added foreign keys

--- database/seedDB.sql
+++ database/seedDB.sql
@@ -5,13 +5,13 @@ CREATE database employee_tracking;
 USE employee_tracking;
 -- create department table
 create table DEPARTMENTS (
-    id int auto_increment not null primary key,
+    id int not null  auto_increment primary key,
     department_name Varchar (30) not null,
     manager_name varchar (20)
 ) 
 -- populate with sample data
-insert into DEPARTMENTS (id, department_name, manager_name)
-values (1, 'Human Resources', 'Pam Poovey'), (2, 'Accounting', 'Cyril Figgis'), (3, 'Research', 'Dr.Krieger'), (4, 'Daily Operations', 'Ray Gillette'), (5,'*Top Secret*', 'Mallory Archer')
+insert into DEPARTMENTS (department_name, manager_name)
+values ('Human Resources', 'Pam Poovey'), ('Accounting', 'Cyril Figgis'), ('Research', 'Dr.Krieger'), ('Daily Operations', 'Ray Gillette'), ('*Top Secret*', 'Mallory Archer')
 
 
 CREATE table ROLES (
@@ -19,13 +19,18 @@ id int not null auto_increment PRIMARY KEY,
 title VARCHAR(30) not null,
 salary decimal(8,2) not null,
 department_id int not null-- will import from department
+FOREIGN KEY (department_id) REFERENCES department(id)
 )
-insert into ROLES (id, title, salary, department_id)
+insert into ROLES (title, salary, department_id)
 values (1, 'Director of Human Resources', 60,000.00, 1), (2, 'Chief Accounting Officer', 60,000.00, 2), (3, 'Head Researcher', 40,000.00, 4), (4, 'Office Manager', 45,000.00, 4), (5, 'Secret Agent', 120,000.00, 5)
+
 create table EMPLOYEES (
    id int not null auto_increment primary key,
    first_name varchar(30) not null,
    last_name varchar(30) not null,
    role_id int not null,-- id will import from roles tables
    manager_id int -- may be null if no manager
+   FOREIGN KEY (manager_id) REFERENCES employee(id)
 )
+insert into EMPLOYEES (id, first_name,last_name, role_id, manager_id)
+values (id, 'Sterling', 'Archer', 5, 5 )
