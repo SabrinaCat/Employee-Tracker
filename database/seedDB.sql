a51a86c4d6ec7909773b874b83dc148048f84534
SabrinaCat <Sabrina.glow22@gmail.com>
2020-07-17 21:37:19 -0500

    PlaceHolder Data integrated into tables

--- database/seedDB.sql
+++ database/seedDB.sql
@@ -7,11 +7,11 @@ USE employee_tracking;
 create table DEPARTMENTS (
     id int not null  auto_increment primary key,
     department_name Varchar (30) not null,
-    manager_name varchar (20)
+    manager_name varchar (30)
 ) 
 -- populate with sample data
-insert into DEPARTMENTS (department_name, manager_name)
-values ('Human Resources', 'Pam Poovey'), ('Accounting', 'Cyril Figgis'), ('Research', 'Dr.Krieger'), ('Daily Operations', 'Ray Gillette'), ('*Top Secret*', 'Mallory Archer')
+insert into DEPARTMENTS (id, department_name, manager_name)
+values (1,'Human Resources', 'Pam Poovey'), (2,'Accounting', 'Cyril Figgis'), (3,'Research'), (4,'Daily Operations', 'Ray Gillette'), (5,'*Top Secret*', 'Mallory Archer')
 
 
 CREATE table ROLES (
@@ -22,15 +22,18 @@ department_id int not null-- will import from department
 FOREIGN KEY (department_id) REFERENCES department(id)
 )
 insert into ROLES (title, salary, department_id)
-values (1, 'Director of Human Resources', 60,000.00, 1), (2, 'Chief Accounting Officer', 60,000.00, 2), (3, 'Head Researcher', 40,000.00, 4), (4, 'Office Manager', 45,000.00, 4), (5, 'Secret Agent', 120,000.00, 5)
+values (1, 'Human Resources', 60,000.00, 1), (2, 'Accounting', 60,000.00, 2), (3, 'Research and Dev', 40,000.00, 4), (4, 'Office Worker', 45,000.00, 4), (5, 'Secret Agent', 120,000.00, 5)
 
 create table EMPLOYEES (
    id int not null auto_increment primary key,
    first_name varchar(30) not null,
    last_name varchar(30) not null,
    role_id int not null,-- id will import from roles tables
-   manager_id int -- may be null if no manager
+   manager_id int -- may be null if not a manager
    FOREIGN KEY (manager_id) REFERENCES employee(id)
 )
 insert into EMPLOYEES (id, first_name,last_name, role_id, manager_id)
-values (id, 'Sterling', 'Archer', 5, 5 )
+values (1, 'Mallory', 'Archer', 5, 1), (2, 'Pam', 'Poovey', 1, 2), (3, 'Cyril', 'Figgis', 2, 3)
+
+insert into EMPLOYEES ( id, first_name, last_name, role_id) 
+values (3, 'Sterling', 'Archer', 5), (4, 'Lana', 'Kane', 5), (5, 'Ray', 'Gillette', 4), (6, 'Dr. Hans', 'Krieger',4), (7, 'Cheryl/Carol/Charlene', 'Tunt', 4)
