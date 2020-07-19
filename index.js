417599f27f84aa3de2be65e3f902fa03c44e5754
SabrinaCat <Sabrina.glow22@gmail.com>
2020-07-19 06:49:58 -0500

    server connection set and tested with mysql and express

---
+++ index.js
@@ -0,0 +1,36 @@
+const express= require('express');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const app = express();
+//set the port
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Sushicat1001",
+  database: "employee_tracking"
+});
+//console.log error or success
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
+
